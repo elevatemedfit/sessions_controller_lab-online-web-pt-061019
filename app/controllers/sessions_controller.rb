295185824a680aaf3e3c628ca2846fc0ff071204
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 15:11:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name].nil? or params[:name].empty?
+    if !!params[:name] and params[:name] !=""
+      # params[:name].nil? or params[:name].empty?
       redirect_to root_path
     else
     session[:name] = params[:name]
