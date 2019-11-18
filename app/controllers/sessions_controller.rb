0fc0eda4df7bbea61f56fcff92b5ff47b3d98afb
Chris Junker <elevatemedfit@gmail.com>
2019-11-18 14:55:25 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if [:name] = nil?
+    if :name = nil?
       redirect_to '/login'
     else
     session[:name] = params[:name]
